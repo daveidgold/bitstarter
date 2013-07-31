b19d79a45b68e5b5a6c0d8ecc2f0954b1fb11e3b
daveidgold <david.gold.it@gmail.com>
2013-07-31 13:59:05 +0000

    final grader code as of end of hw3

--- grader.js
+++ grader.js
@@ -28,6 +28,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://gentle-badlands-1337.herokuapp.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -67,6 +68,7 @@ if(require.main == module) {
     program
 	.option('-c,  --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_file>','Path to http://gentle-badlands-1337.herokuapp.com', URL_DEFAULT)
 	.parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
