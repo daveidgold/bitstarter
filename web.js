c573c353fe178c07285a7ee38345efb2e09af2f8
Ubuntu <ubuntu@ip-172-31-46-251.us-west-2.compute.internal>
2013-08-06 13:35:11 +0000

    Updated Product description.

--- web.js
+++ web.js
@@ -1,16 +1,15 @@
-var fs = require('fs');
 var express = require('express');
-
+var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
-var data = fs.readFileSync("index.html", "utf-8");
-
 app.get('/', function(request, response) {
-  response.send(data);
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
